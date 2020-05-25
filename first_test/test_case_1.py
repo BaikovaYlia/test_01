244488a8946df7551086d128c1070fbe40347900
AlexDiatlov <alexdiatlov@gmail.com>
2020-05-25 14:11:39 +0300

    початкові правки, файл пайтон не називають кирилицею!!!!!!!

--- first_test/test_case_1.py
+++ first_test/test_case_1.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-import  unittest
+import unittest
 from selenium.webdriver.common.keys import Keys
-import  time
+import time
+
 
 class GoogleSearch(unittest.TestCase):
     def setUp(self):
@@ -10,28 +11,29 @@ class GoogleSearch(unittest.TestCase):
 
         def test_01(self):
             driver = self.driver
-            input_fild = driver.find_element_by_id('lst-id')
-            input_fild.send_keys('python')
-            input_fild.send_keys(Keys.ENTER)
-
+            input_field = driver.find_element_by_id('lst-id')
+            input_field.send_keys('python')
+            input_field.send_keys(Keys.ENTER)
             time.sleep(2)
 
             titles = driver.find_element_by_class_name('r')
             for title in titles:
                 assert 'python' in title.text.lower()
+                
         def teat_02(self):
             driver = self.driver
-            input_fild = driver.find_element_by_id('lst-id')
-            input_fild.send_keys('python')
+            input_field = driver.find_element_by_id('lst-id')
+            input_field.send_keys('python')
             time.sleep(2)
-            input_fild.send_keys(Keys.DOWN)
-            input_fild.send_keys(Keys.ENTER)
+            input_field.send_keys(Keys.DOWN)
+            input_field.send_keys(Keys.ENTER)
 
             time.sleep(2)
 
             titles = driver.find_element_by_class_name('r')
             for title in titles:
                 assert 'python' in title.text.lower()
+                
         def tearDown(self):
             self.driver.quit()
 
